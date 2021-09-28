c4c542cb81411cd7387528ab71121044d88f6c89
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-09-28 14:50:51 -0700

    GET list of entities

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 
+const { ghibliFilms } = require ('./data.js')
+
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/studio-ghibli', (req, res) => {
+  res.json({ghibliFilms})
 })
+
+app.get('/studio-ghibli/:id', (req, res) => {
+    res.json({
+        id: req.params.id
+    })
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
